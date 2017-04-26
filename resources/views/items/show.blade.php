8c9356b9e66d2e95aa739fbebd3509d4197bb220
marek1981 <mvalentic1981@gmail.com>
2017-04-26 20:50:38 +0200

    Dodan view show itema

---
+++ resources/views/items/show.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title>Laravel projekt</title>
+    </head>
+    <body>
+        <h2>{{$item->title}}</h2>
+		<div>{{$item->content}}</div>
+    </body>
+</html>
